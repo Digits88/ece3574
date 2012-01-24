bc580a9563522a8babf2ecbe68a3c5dd600967be
hailemariamag@gmail.com <hailemariamag@gmail.com@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 20:01:17 +0000

    v1

---
+++ assignments/alazar_hailemariam/alazarHailemariam/src/org/alazar/hailemariam/AlazarHailemariamActivity.java
@@ -0,0 +1,13 @@
+package org.alazar.hailemariam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlazarHailemariamActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
