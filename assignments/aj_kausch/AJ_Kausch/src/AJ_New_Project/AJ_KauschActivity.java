f91f72c0bc00f2ebd56454aafde6356557656046
uhawks66 <uhawks66@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 20:00:49 +0000

    Test Commit

---
+++ assignments/aj_kausch/AJ_Kausch/src/AJ_New_Project/AJ_KauschActivity.java
@@ -0,0 +1,13 @@
+package AJ_New_Project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AJ_KauschActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
