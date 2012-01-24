fda2209a20e4a13990812cc346727f7a4061e089
steve.mattoon@gmail.com <steve.mattoon@gmail.com@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 19:59:15 +0000

    Initial commit of this project

---
+++ assignments/Steve_Mattoon/SteveMattoon/src/org/steve/mattoon/SteveMattoonActivity.java
@@ -0,0 +1,13 @@
+package org.steve.mattoon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SteveMattoonActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
