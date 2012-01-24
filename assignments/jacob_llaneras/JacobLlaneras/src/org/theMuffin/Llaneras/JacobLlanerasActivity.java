121453f8a8f45893f99a53e42855d67bd5cc6ecf
jrainbowmuffin <jrainbowmuffin@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 19:59:13 +0000

    Initial Commit

---
+++ assignments/jacob_llaneras/JacobLlaneras/src/org/theMuffin/Llaneras/JacobLlanerasActivity.java
@@ -0,0 +1,13 @@
+package org.theMuffin.Llaneras;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JacobLlanerasActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
