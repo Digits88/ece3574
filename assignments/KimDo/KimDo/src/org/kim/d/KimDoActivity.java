a30639ad29a155e59ba8c8c19b5540fbace8d8af
kimdo0305 <kimdo0305@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 21:04:03 +0000

    initiating version

---
+++ assignments/KimDo/KimDo/src/org/kim/d/KimDoActivity.java
@@ -0,0 +1,13 @@
+package org.kim.d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KimDoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
