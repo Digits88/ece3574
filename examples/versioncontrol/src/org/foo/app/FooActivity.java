13c6d44e247f04ba3f95b77f3d68a52ee9eec350
jules.white@gmail.com <jules.white@gmail.com@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 19:40:59 +0000

    A minor change to explain commits

--- examples/versioncontrol/src/org/foo/app/FooActivity.java
+++ examples/versioncontrol/src/org/foo/app/FooActivity.java
@@ -2,14 +2,19 @@ package org.foo.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FooActivity extends Activity {
 	
+	
+	private static final String TAG = Activity.class.getName();
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Log.v(TAG, "A minor change.");
     }
 }
