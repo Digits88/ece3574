4e7a7683dc2a4d36f761f65b6fbf106828d339ff
accorvin <accorvin@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-24 19:59:13 +0000

    Initial commit of this project

---
+++ assignments/alex_corvin/src/org/alex/corvin/AlexCorvinActivity.java
@@ -0,0 +1,13 @@
+package org.alex.corvin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlexCorvinActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
