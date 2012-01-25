dabc931ade4ec5519bd0c9cbc9d7a4c9db292c98
ngentzen@gmail.com <ngentzen@gmail.com@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-25 02:06:34 +0000

    Changed "World" to "Satan"

---
+++ assignments/nathan_gentzen/testProject/testProject.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : testProject.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int b = 6;
+	int b
+	puts("Hello Satan!!!");
+	return EXIT_SUCCESS;
+}
