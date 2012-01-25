0e0178586d298fcd29eebf3d8490c0440d016c50
agood25 <agood25@7215e1fe-2768-82d7-5b23-1873a59a785d>
2012-01-25 01:33:22 +0000

    Initial import.

---
+++ assignments/aaron_good/AaronGood/src/aaron/good/AaronGoodActivity.java
@@ -0,0 +1,13 @@
+package aaron.good;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AaronGoodActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
